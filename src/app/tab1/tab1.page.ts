9b98ea32aec9e1f5512911fb61e8d06685c696c7
Jeffeson Gonçalves <jeffesongonsalvespereira@gmail.com>
2019-09-28 19:38:09 -0300

    Tela Inicial

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,31 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public obj = {
+    "bolos": [
+      {
+        "titulo": "Bolo de chocolate",
+        "preco": "apenas R$ 50",
+        "descricao": "Bolo vulcão de chocolate com uma calda incrível."
+      },
+      {
+        "titulo": "Bolo de corbetura de morango",
+        "preco": "Preço R$ 60",
+        "descricao": "Bolo de corbertura de morango."
+      },
+      {
+        "titulo": "Bolo de chocolate",
+        "preco": "apenas R$ 50",
+        "descricao": "Bolo de chocolate "
+      },
+      {
+        "titulo": "Bolo de Limão",
+        "preco": "apenas R$ 45",
+        "descricao": "Bolo de Limaão."
+      }
+    ]
+  }
+
+  constructor() { }
 
 }
