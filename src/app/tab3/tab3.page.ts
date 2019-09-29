5173fe3509ea153b0c710168965c508784994d28
Jeffeson Gonçalves <jeffesongonsalvespereira@gmail.com>
2019-09-28 21:53:34 -0300

    Final Commit

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,26 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor( public loadingController: LoadingController) {}
+
+  async fecharLoad() {
+    const loading = await this.loadingController.create({
+      duration: 2000
+    });
+    await loading.present();
+
+    const { role, data } = await loading.onDidDismiss();
+  }
+
+  async abrirLoad() {
+    const loading = await this.loadingController.create({
+      spinner: null,
+      duration: 5000,
+      message: 'Enviando...',
+      translucent: true,
+      cssClass: 'custom-class custom-loading'
+    });
+    return await loading.present();
+  }
 
 }
